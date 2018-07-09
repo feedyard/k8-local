07d9bca8b710101887dc0564b5a6cbc9ca0e9a04
ncheneweth <nchenewe@thoughtworks.com>
2018-07-09 18:29:11 -0500

    observe commits

--- tasks.py
+++ tasks.py
@@ -1,10 +1,11 @@
 from invoke import task
+import time
 
 @task
 def init(ctx):
-    print('install CoreDNS')
-    ctx.run('bash ./dns/update_dns.sh | kubectl apply -f -')
-    ctx.run('kubectl delete --namespace=kube-system deployment kube-dns')
+    # print('install CoreDNS')
+    # ctx.run('bash ./dns/update_dns.sh | kubectl apply -f -')
+    # ctx.run('kubectl delete --namespace=kube-system deployment kube-dns')
 
     print('apply metrics-server and heapster based hpa')
     ctx.run('kubectl apply -f metrics-server/')
@@ -23,7 +24,9 @@ def core(ctx):
     print('deploy observability tools')
     ctx.run('kubectl apply -f observe/namespaces.yaml') # defines the following: "monitoring"
     ctx.run('kubectl apply -f observe/elasticsearch/')
+    time.sleep(90)
     ctx.run('kubectl apply -f observe/fluentd/')
+    time.sleep(60)
     ctx.run('kubectl apply -f observe/kibana/')
     ctx.run('cat observe/usage.txt')
 
@@ -31,7 +34,9 @@ def core(ctx):
 def charts(ctx):
     print('some core features still managed with charts')
     ctx.run('helm init')
+    time.sleep(20)
     ctx.run('helm install stable/prometheus --name prometheus')
+    time.sleep(90)
     ctx.run('helm install stable/grafana --name grafana -f observe/charts/grafana-values.yaml')
 
 @task
