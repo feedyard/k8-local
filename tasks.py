09da9dee6678514ffd9d2b6edb92465ac919fb4b
ncheneweth <nchenewe@thoughtworks.com>
2018-07-14 15:35:30 -0500

    update

--- tasks.py
+++ tasks.py
@@ -1,43 +1,49 @@
 from invoke import task
 import time
 
-@task
-def init(ctx):
-    # print('install CoreDNS')
-    # ctx.run('bash ./dns/update_dns.sh | kubectl apply -f -')
-    # ctx.run('kubectl delete --namespace=kube-system deployment kube-dns')
+@task(optional=['edge'])
+def init(ctx, edge=False):
 
-    print('apply metrics-server and heapster based hpa')
-    ctx.run('kubectl apply -f metrics-server/')
-    ctx.run('kubectl apply -f heapster/')
+    if edge:
+        print('includes edge options')
+        ctx.run('bash ./dns/update_dns.sh | kubectl apply -f -')
+        ctx.run('kubectl delete --namespace=kube-system deployment kube-dns')
+
+    print('initial config')
+    ctx.run('kubectl apply -f metrics-server-1.8+/')
+    ctx.run('kubectl apply -f dashboard/')
+    ctx.run('kubectl describe svc kubernetes-dashboard -n kube-system')
+
+@task(optional=['edge'])
+def monitor(ctx, edge=False):
+
+    if edge:
+        print('no edge options for monitoring yet')
+
+    print('deploy monitoring tools')
+    ctx.run('kubectl apply -f monitoring/monitoring-namespace.yaml')
+    ctx.run('kubectl apply -f monitoring/heapster/influxdb/')
+    time.sleep(20)
+    ctx.run('kubectl apply -f monitoring/heapster/heapster/')
+    time.sleep(20)
+    ctx.run('kubectl apply -f monitoring/heapster/grafana/')
+    ctx.run('kubectl describe svc grafana -n monitoring')
+    ctx.run('kubectl apply -f monitoring/elasticsearch/')
+    time.sleep(90)
+    ctx.run('kubectl apply -f monitoring/fluentd/')
+    time.sleep(60)
+    ctx.run('kubectl apply -f monitoring/kibana/')
+    ctx.run('kubectl describe svc kibana -n monitoring')
+
+@task(optional=['edge'])
+def ingress(ctx, edge=False):
+
+    if edge:
+        print('no edge options for ingress yet')
 
     print('deploy traefik ingress controller')
     ctx.run('kubectl apply -f ingress/')
     ctx.run('kubectl describe svc -n kube-system traefik-ingress-service')
-    ctx.run('cat ingress/usage.txt')
-
-    print('deploy kubernetes dashboard')
-    ctx.run('kubectl apply -f dashboard/')
-
-@task
-def core(ctx):
-    print('deploy observability tools')
-    ctx.run('kubectl apply -f observe/namespaces.yaml') # defines the following: "monitoring"
-    ctx.run('kubectl apply -f observe/elasticsearch/')
-    time.sleep(90)
-    ctx.run('kubectl apply -f observe/fluentd/')
-    time.sleep(60)
-    ctx.run('kubectl apply -f observe/kibana/')
-    ctx.run('cat observe/usage.txt')
-
-@task
-def charts(ctx):
-    print('some core features still managed with charts')
-    ctx.run('helm init')
-    time.sleep(20)
-    ctx.run('helm install stable/prometheus --name prometheus')
-    time.sleep(90)
-    ctx.run('helm install stable/grafana --name grafana -f observe/charts/grafana-values.yaml')
 
 @task
 def test(ctx):
